36f36e4fa3daf7c614ec59e1cc60deaf5d4e9954
Genia V <evgenia.verhovtseva@yandex.ru>
2022-01-16 22:38:03 +0300

    Доработка способа оплаты

--- script1.js
+++ script1.js
@@ -2,24 +2,13 @@
 const popupBuyTicket = document.querySelector('#popup__buy-ticket'); // попап
 const popupBuyTicketForm = popupBuyTicket.querySelector('#buy-ticket') // форма попапа
 const userEmail = popupBuyTicket.querySelector('.popup__email-input');
-const payMethod = popupBuyTicket.querySelector('input[name="payment"]:checked');
 const priceTicket = popupBuyTicketForm.querySelector('.popup__price');
 const quantityField = popupBuyTicketForm.querySelector('.popup__count-total-result');
 
 const plusButton = popupBuyTicketForm.querySelector('.popup__count-button-plus');
 const minusButton = popupBuyTicketForm.querySelector('.popup__count-button-minus');
-let count = 0;
-
 
 // исходные значения
-const requestValues = {
-	countTicket: quantityField.value,
-	price: priceTicket.textContent,
-	email: userEmail.value,
-	methodpay: payMethod.value
-};
-console.log(requestValues)
-
 function openPopup (popup) {
 	popup.classList.add('popup_opened')
   }
@@ -39,18 +28,20 @@ popupBuyTicketForm.addEventListener('submit', (evt) => {
 
 minusButton.addEventListener('click', function () {
 	this.nextElementSibling.stepDown();
-	
+
 })
 
 plusButton.addEventListener('click', function () {
 	this.previousElementSibling.stepUp();
-	
+
 })
 
 
 popupBuyTicketForm.addEventListener("submit", (evt) => {
 	evt.preventDefault();
-    
+
+  const payMethod = popupBuyTicket.querySelector('input[name="payment"]:checked');
+
 	const requestValues = {
 		countTicket: quantityField.value,
 		price: 500 * quantityField.value + '',
